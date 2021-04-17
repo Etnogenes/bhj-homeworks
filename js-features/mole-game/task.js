3113183cd07f605ab72f503db5a15ec001d3177d
Etnogenes <andrei09.94@mail.ru>
2021-04-18 01:28:05 +0300

    don't understand anything

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const mole = document.getElementsByClassName("hole_has-mole");
+const hole = document.getElementsByClassName("hole");
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+
+
+
+
+
+
+
+for (let i = 1, i <= hole.length, i++) {
+
+const doSomething = function() {
+	dead.textContent ++;
+
+
+
+}
+
+
+}
+
+
+
+hole.onclick = doSomething;
+
+
+
+
