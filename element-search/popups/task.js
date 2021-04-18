2c3f15d0bf85f7c5a2cdddd883fd3b6092678647
Etnogenes <andrei09.94@mail.ru>
2021-04-19 00:04:31 +0300

    some more

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -6,14 +6,19 @@ const success1 = document.getElementsByClassName("show-success");
 const success2 = document.getElementById("modal_success");
 
 const letClose = function() {
-	modal_main.className() = "modal";
+	modal_main.classList.remove("modal_active");
 }
 
 close.onclick = letClose;
 
 
 const letSuccess = function() {
-	success2.className() = "modal modal_active";
+	success2.classList.add("modal_active");
 }
 
 success1.onclick = letSuccess;
+
+
+
+/*className, равно как и classList.add/remove, почему-то не помогают.
+Я знаю, что это псевдомассив. Неужели, свойство не соответствует атрибуту?*/
