a50111ec13d1a5242a09d067ccdbd16e6b9fa5cf
Etnogenes <andrei09.94@mail.ru>
2021-04-21 20:08:10 +0300

    just a little bit

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,24 +1,27 @@
 "use strcit";
 
 const modal_main = document.getElementById("modal_main");
-const close = document.getElementsByClassName("modal__close");
-const success1 = document.getElementsByClassName("show-success");
+const close = document.getElementsByClassName("modal__close modal__close_times");
+const success1 = document.getElementsByClassName("btn btn_danger modal__close show-success");
 const success2 = document.getElementById("modal_success");
+
+let arr1 = Array.from(close);
+let elementClose = arr1[0];
 
 const letClose = function() {
 	modal_main.classList.remove("modal_active");
+	success2.classList.remove("modal_active");
 }
 
-close.onclick = letClose;
+elementClose.onclick = letClose;
 
+let arr2 = Array.from(success1);
+let elementSuccess = arr2[0];
 
 const letSuccess = function() {
 	success2.classList.add("modal_active");
 }
 
-success1.onclick = letSuccess;
+elementSuccess.onclick = letSuccess;
 
 
-
-/*className, равно как и classList.add/remove, почему-то не помогают.
-Я знаю, что это псевдомассив. Неужели, свойство не соответствует атрибуту?*/
