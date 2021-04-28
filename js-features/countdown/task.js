d957f214a629924e264bae5926c3e0135b8d60b6
Etnogenes <andrei09.94@mail.ru>
2021-04-28 12:57:04 +0300

    Finaly

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -4,17 +4,11 @@ let timer = document.getElementById("timer");
 
 const niceTimer = function() {
 	timer.textContent --;
+
+	if (timer.textContent == 0) {
+		clearInterval(niceTimer);
+		alert("Вы победили??");
+	}
 }
 
 setInterval(niceTimer, 100);
-
-/*if (timer === 0) {
-	clearInterval(niceTimer);
-	alert("Вы победили??");
-}*/
-
-setTimeout(() => {
-	clearInterval(niceTimer);
-	alert("Вы победили?");
-}, timer === 0);
-
