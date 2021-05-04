eb1b5143d88ca07e166f51ec165d66162fc953e8
Etnogenes <andrei09.94@mail.ru>
2021-05-04 21:57:52 +0300

    I'm surrened

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -8,34 +8,27 @@ const lost = document.getElementById("lost");
 
 let arr1 = Array.from(hole);
 
+for(let i = 0; i <= arr1.length; i++) {
+
+
+	const getHole = i => document.getElementById(`hole${i}`);
+}
+
+
 const addKill = function() {
 
-	for(let i = 0; i <= arr1.length; i++) {
-
-		if (arr1[i].className.includes("hole_has-mole")) {
-			dead.textContent ++;
-		} else if (dead.textContent == 10) {
-			alert("Вы выиграли?");
-		} else if (arr1[i].classList.contains("hole_has-mole") == false) {
-			lost.textContent ++;
-		} else if (lost.textContent == 5) {
-			alert("Вы проиграли!");
-		}
+	if (arr1[0].className.includes("hole_has-mole")) {
+		dead.textContent ++;
+	} else if (arr1[0].classList.contains("hole_has-mole") == false) {
+		lost.textContent ++;
 	}
 }
 
-hole.onclick = addKill;
-
-/* Можно объявить переменную hole иначе:
-
-let index = 0;
-const hole = document.getElementById("hole${index}");   //В этой строчке почему-то не активна переменная index
-
-const addKill = function() {
-	for(index; index <= arr1.length; index++) {
- 	// конструкция if
-	}
+if (dead.textContent == 10) {
+	alert("Вы выиграли?");
+} else if (lost.textContent == 5) {
+	alert("Вы проиграли!");
 }
-hole.onclick = addKill;
 
-*/
+
+hole[0].onclick = addKill;
