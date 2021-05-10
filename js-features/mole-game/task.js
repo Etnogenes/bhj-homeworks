00a1e8612bd615a5445d30e5436a0bbd6c5388db
Etnogenes <andrei09.94@mail.ru>
2021-05-10 21:25:16 +0300

    I feel so helpless

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -10,25 +10,15 @@ let arr1 = Array.from(hole);
 
 for(let i = 0; i <= arr1.length; i++) {
 
-
-	const getHole = i => document.getElementById(`hole${i}`);
-}
-
-
-const addKill = function() {
-
-	if (arr1[0].className.includes("hole_has-mole")) {
-		dead.textContent ++;
-	} else if (arr1[0].classList.contains("hole_has-mole") == false) {
-		lost.textContent ++;
+	hole[i].onclick = function(click) {
+		if (arr1[i].className.includes("hole_has-mole")) {
+			dead.textContent ++;
+		} else if (arr1[i].classList.contains("hole_has-mole") == false) {
+			lost.textContent ++;
+		} else if (dead.textContent == 10) {
+			alert("Вы выиграли?");
+		} else if (lost.textContent == 5) {
+		alert("Вы проиграли!");
+		}
 	}
 }
-
-if (dead.textContent == 10) {
-	alert("Вы выиграли?");
-} else if (lost.textContent == 5) {
-	alert("Вы проиграли!");
-}
-
-
-hole[0].onclick = addKill;
