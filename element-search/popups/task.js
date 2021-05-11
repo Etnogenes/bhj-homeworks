9f2e85dd4027b7b34e26a8e56109c652830831d5
Etnogenes <andrei09.94@mail.ru>
2021-05-11 21:58:12 +0300

    am

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,31 +1,28 @@
 "use strict";
 
-const modal_main = document.getElementById("modal_main");
+const modalMain = document.getElementById("modal_main");
 const close = document.getElementsByClassName("modal__close modal__close_times");
 const success1 = document.getElementsByClassName("btn btn_danger modal__close show-success");
 const success2 = document.getElementById("modal_success");
 
-modal_main.classList.add("modal_active");
+modalMain.classList.add("modal_active");
 
 let arr1 = Array.from(close);
-//let elementClose = arr1[0];
 
-const letClose = function() {
-	modal_main.classList.remove("modal_active");
-	success2.classList.remove("modal_active");
+for (let i = 0; i <= arr1.length; i++) {
+	arr1[i].onclick = function(click) {
+		this.closest('.modal').classList.remove('modal_active');
+	};
+	//alert(i);
 }
 
-//elementClose.onclick = letClose;
-arr1[0].addEventListener("click", letClose);
-arr1[1].addEventListener("click", letClose);
-
-
-let arr2 = Array.from(success1);
+let arr2 = Array.from(success1);  // Эту строчку интерпретатор не может определить, а должен
 let elementSuccess = arr2[0];
+
 
 const letSuccess = function() {
 	success2.classList.add("modal_active");
-	modal_main.classList.remove("modal_active");
+	modalMain.classList.remove("modal_active");
 }
 
 elementSuccess.onclick = letSuccess;
