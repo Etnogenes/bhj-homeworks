e82dbe0916fe21359e5186fe2258d23bb6b57f36
Etnogenes <andrei09.94@mail.ru>
2021-05-11 21:59:21 +0300

    fed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,33 +1,41 @@
 "use strict";
 
-const slider__item = document.getElementsByClassName("slider__item");
-const slider__arrow = document.getElementsByClassName("slider__arrow");
-const slider__dot = document.getElementsByClassName("slider__dot");
+const sliderItem = document.getElementsByClassName("slider__item");
+const sliderArrow = document.getElementsByClassName("slider__arrow");
+const sliderDot = document.getElementsByClassName("slider__dot");
 
 
-let arr1 = Array.from(slider__item);
-let arr2 = Array.from(slider__arrow);
-let arr3 = Array.from(slider__dot);
+let arr1 = Array.from(sliderItem);
+let arr2 = Array.from(sliderArrow);
+let arr3 = Array.from(sliderDot);
 
-let element_arr2 = arr2[0];
-let element_arr3 = arr3[1];
+let valueSlide = 0; 
 
-const switch_slider__arrow = function() {
-	for(let i = 0; i <= arr1.length; i++) {
-		arr1[i].classList.remove("slider__item_active");
-		arr1[i + 1].classList.add("slider__item_active");
-	}
+function switchSlide(valueSlide) {
+	
+	let activeSlide = arr1.findIndex(item => item.className === "slider__item slider__item_active");
+	
+	if (arr1[activeSlide].classList.contains("slider__item_active") == true) {
+		arr1[activeSlide].classList.remove("slider__item_active");
+	};
+
+	arr1[valueSlide].classList.add("slider__item_active");
 }
 
 
-const switch_slider__dot = function() {
+const switchDots = function() {
+	switchSlide();
+}
+
+const switchArrow = function() {
+
+	//найти позицию активного слайда
+	let result = (arr1.findIndex(item => item.className === "slider__item slider__item_active") > 3) ? 0 : 4;
+
+	//вызов функции
+	switchSlide(result);
 
 }
 
-
-element_arr2.onclick = switch_slider__arrow;
-element_arr3.onclick = switch_slider__dot;
-
-
-
-
+arr2[1].onclick = switchArrow;
+arr3.onclick = switchDots;
