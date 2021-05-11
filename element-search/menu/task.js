4ac12af09a4968bdbaafb800beadf6e8e63b3118
Etnogenes <andrei09.94@mail.ru>
2021-05-11 21:57:05 +0300

    I

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,26 +1,18 @@
 "use strict";
 
-const menu_main = document.getElementsByClassName("menu menu_main");
-const menu__item = document.querySelectorAll("ul.menu menu_main li");
-const menu__link = document.querySelectorAll("li.menu__item a");
-const menu_sub = document.querySelectorAll("li.menu__item ul");
+const menuLink = document.querySelectorAll("li.menu__item a");
+const menuSub = document.querySelectorAll("li.menu__item ul");
 
 
-let arr1 = Array.from(menu__item);
-let arr2 = Array.from(menu__link);
-let arr3 = Array.from(menu_sub);
+let arr1 = Array.from(menuLink);
+let arr2 = Array.from(menuSub);
 
-let elementMenu__link = arr2[1];
 
-for (let i = 0; i <= arr2.length; i++) {
-	if (menu__item.closest("ul") == true) {
-		const show_menu = function() {
-			menu_sub.classList.toggle("menu_active");
-			return false;
+for (let i = 0; i <= arr1.length; i++) {
+	arr1[i].onclick = function(click) {
+		if (menuLink.closest(".menu_sub") /*== ul*/) {
+			arr2.classList.toggle("menu_active");
 		}
+		return false;
 	}
 }
-
-
-elementMenu__link.onclick = show_menu;
-
