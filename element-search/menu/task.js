5ebc43b4a59c5ef31aff4a6f55d7416384c1a835
Etnogenes <andrei09.94@mail.ru>
2021-05-20 00:37:11 +0300

    SURRENDED

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,16 +2,17 @@
 
 const menuLink = document.querySelectorAll("li.menu__item a");
 const menuSub = document.querySelectorAll("li.menu__item ul");
+const menuItem = document.getElementsByClassName("menu__item");
 
 
 let arr1 = Array.from(menuLink);
 let arr2 = Array.from(menuSub);
+let arr3 = Array.from(menuItem);
 
-
-for (let i = 0; i <= arr1.length; i++) {
+for (let i = 0; i < arr1.length; i++) {
 	arr1[i].onclick = function(click) {
-		if (menuLink.closest(".menu_sub") /*== ul*/) {
-			arr2.classList.toggle("menu_active");
+		if (arr3[i].contains(arr2[0]) == true) {
+			arr2[0].classList.toggle("menu_active");
 		}
 		return false;
 	}
