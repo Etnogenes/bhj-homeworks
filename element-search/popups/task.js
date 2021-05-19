ccd1d605ca21099276449ecc3c80150512a74f2e
Etnogenes <andrei09.94@mail.ru>
2021-05-19 23:46:39 +0300

    All ringht here

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -9,14 +9,13 @@ modalMain.classList.add("modal_active");
 
 let arr1 = Array.from(close);
 
-for (let i = 0; i <= arr1.length; i++) {
+for (let i = 0; i < arr1.length; i++) {
 	arr1[i].onclick = function(click) {
 		this.closest('.modal').classList.remove('modal_active');
 	};
-	//alert(i);
 }
 
-let arr2 = Array.from(success1);  // Эту строчку интерпретатор не может определить, а должен
+let arr2 = Array.from(success1);
 let elementSuccess = arr2[0];
 
 
