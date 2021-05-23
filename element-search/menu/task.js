298575d8995f4884e244c9187f92976473b738c3
Etnogenes <andrei09.94@mail.ru>
2021-05-23 21:25:01 +0300

    I SENSE

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,14 +5,14 @@ const menuSub = document.querySelectorAll("li.menu__item ul");
 const menuItem = document.getElementsByClassName("menu__item");
 
 
-let arr1 = Array.from(menuLink);
-let arr2 = Array.from(menuSub);
-let arr3 = Array.from(menuItem);
+let arrLink = Array.from(menuLink);
+let arrSub = Array.from(menuSub);
+let arrItem = Array.from(menuItem);
 
-for (let i = 0; i < arr1.length; i++) {
-	arr1[i].onclick = function(click) {
-		if (arr3[i].contains(arr2[0]) == true) {
-			arr2[0].classList.toggle("menu_active");
+for (let i = 0; i < arrLink.length; i++) {
+	arrLink[i].onclick = function(click) {
+		if (arrLink[i].closest(".menu__item") && arrItem[i].contains(arrSub[i])) {
+			this.closest(".menu__item").contains(arrSub[0].className.toggle("menu_active"));
 		}
 		return false;
 	}
