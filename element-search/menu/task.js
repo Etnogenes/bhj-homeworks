d7d8fb0b3482257a5aeb1a3d551370222fd1b701
Etnogenes <andrei09.94@mail.ru>
2021-05-24 02:02:05 +0300

    Y

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,18 +1,13 @@
 "use strict";
 
 const menuLink = document.querySelectorAll("li.menu__item a");
-const menuSub = document.querySelectorAll("li.menu__item ul");
-const menuItem = document.getElementsByClassName("menu__item");
-
 
 let arrLink = Array.from(menuLink);
-let arrSub = Array.from(menuSub);
-let arrItem = Array.from(menuItem);
 
 for (let i = 0; i < arrLink.length; i++) {
 	arrLink[i].onclick = function(click) {
-		if (arrLink[i].closest(".menu__item") && arrItem[i].contains(arrSub[i])) {
-			this.closest(".menu__item").contains(arrSub[i].classList.toggle("menu_active"));
+		if (arrLink[i].closest(".menu__item").querySelector(".menu_sub")) {
+			this.closest(".menu__item").querySelector(".menu_sub").classList.toggle("menu_active");
 		}
 		return false;
 	}
