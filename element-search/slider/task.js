8483f3d9ae5dd91127a5d72be52187a04520ea13
Etnogenes <andrei09.94@mail.ru>
2021-05-29 21:33:45 +0300

    It never end

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,40 +2,31 @@
 
 const sliderItem = document.getElementsByClassName("slider__item");
 const sliderArrow = document.getElementsByClassName("slider__arrow");
-const sliderDot = document.getElementsByClassName("slider__dot");
 
-
-let arr1 = Array.from(sliderItem);
-let arr2 = Array.from(sliderArrow);
-let arr3 = Array.from(sliderDot);
+let arrItem = Array.from(sliderItem);
+let arrArrow = Array.from(sliderArrow);
 
 let valueSlide = 0; 
 
 function switchSlide(valueSlide) {
 	
-	let activeSlide = arr1.findIndex(item => item.className === "slider__item slider__item_active");
+	let activeSlide = arrItem.findIndex(item => item.className == "slider__item slider__item_active");
 	
-	if (arr1[activeSlide].classList.contains("slider__item_active") == true) {
-		arr1[activeSlide].classList.remove("slider__item_active");
+	if (arrItem[activeSlide].classList.contains("slider__item_active") == true) {
+		arrItem[activeSlide].classList.remove("slider__item_active");
 	};
 
-	arr1[valueSlide].classList.add("slider__item_active");
-}
-
-
-const switchDots = function() {
-	switchSlide();
+arrItem[valueSlide].classList.add("slider__item_active");
 }
 
 const switchArrow = function() {
-
 	//найти позицию активного слайда
-	let result = (arr1.findIndex(item => item.className === "slider__item slider__item_active") > 3) ? 0 : 4;
+	let result = arrItem.findIndex(item => item.className == "slider__item slider__item_active") == valueSlide ? (valueSlide + 1) : 0;
 
 	//вызов функции
 	switchSlide(result);
-
 }
 
-arr2[1].onclick = switchArrow;
-arr3.onclick = switchDots;
+for (let i = 0; i < arrArrow.length; i++) {
+	arrArrow[i].onclick = switchArrow;
+}
