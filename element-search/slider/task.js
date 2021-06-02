53fdf1742c7dab29d2a14bd7d035094e21bc6c37
Etnogenes <andrei09.94@mail.ru>
2021-06-02 22:14:28 +0300

    That's all

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,31 +2,42 @@
 
 const sliderItem = document.getElementsByClassName("slider__item");
 const sliderArrow = document.getElementsByClassName("slider__arrow");
+//const sliderDot = document.getElementsByClassName("slider__dot");
 
 let arrItem = Array.from(sliderItem);
 let arrArrow = Array.from(sliderArrow);
+//let arrDots = Array.from(sliderDot);
 
 let valueSlide = 0; 
 
 function switchSlide(valueSlide) {
 	
-	let activeSlide = arrItem.findIndex(item => item.className == "slider__item slider__item_active");
-	
-	if (arrItem[activeSlide].classList.contains("slider__item_active") == true) {
-		arrItem[activeSlide].classList.remove("slider__item_active");
-	};
-
-arrItem[valueSlide].classList.add("slider__item_active");
+	let positionActiveSlide = arrItem.findIndex(item => item.classList.contains("slider__item_active"));
+	arrItem[positionActiveSlide].classList.remove("slider__item_active");
+	arrItem[valueSlide].classList.add("slider__item_active");
+//	arrDots[positionActiveSlide].classList.remove("slider__dot_active");
+//	arrDots[valueSlide].classList.add("slider__dot_active");
 }
 
-const switchArrow = function() {
-	//найти позицию активного слайда
-	let result = (arrItem.findIndex(item => item.className == "slider__item slider__item_active") + 1) < arrItem.length ? (arrItem.findIndex(item => item.className == "slider__item slider__item_active") + 1) : valueSlide;
-
-	//вызов функции
-	switchSlide(result);
+const switchArrowLeft = function() {
+	let positionActiveSlide = arrItem.findIndex(item => item.classList.contains("slider__item_active"));
+	switchSlide((positionActiveSlide) > valueSlide ? (positionActiveSlide - 1) : arrItem.length - 1);
 }
 
-for (let i = 0; i < arrArrow.length; i++) {
-	arrArrow[i].onclick = switchArrow;
+const switchArrowRight = function() {
+	let positionActiveSlide = arrItem.findIndex(item => item.classList.contains("slider__item_active"));
+	switchSlide((positionActiveSlide) < arrItem.length - 1 ? (positionActiveSlide + 1) : valueSlide);
 }
+
+arrArrow[0].onclick = switchArrowLeft;
+arrArrow[1].onclick = switchArrowRight;
+
+/*for (let i = 0; i < arrDots.length; i++) {
+	arrDots[i].onclick = function(click) {
+		this.closest(".slider__dots").querySelectorAll(".slider__dot").classList.remove("slider__dot_active");
+		this.closest(".slider__dot").classList.add("slider__dot_active");
+		let positionActiveDot = arrDots.findIndex(item => item.classList.contains("slider__dot_active"));
+
+		switchSlide(positionActiveDot);
+	}
+}*/
